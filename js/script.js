3dc05c1d6e04099c9102d87f08d8e322fe955658
Jason <jasonbernert@gmail.com>
2016-09-16 15:13:31 -0700

    working weather descriptions

--- js/script.js
+++ js/script.js
@@ -1,35 +1,34 @@
-  // Set up map
-  var Esri_WorldImagery = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-  	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-  });
+// Set up map
+var Esri_WorldImagery = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
 
-  var map = L.map('map', {
-    scrollWheelZoom: false,
-    center: [40, -98],
-    zoom: 4,
-    minZoom: 3,
-    maxZoom: 10
-   });
+var map = L.map('map', {
+  scrollWheelZoom: false,
+  center: [40, -98],
+  zoom: 4,
+  minZoom: 4,
+  maxZoom: 4
+ });
 
-  map.addLayer(Esri_WorldImagery);
+map.addLayer(Esri_WorldImagery);
 
-  // Hiding with jQuery to later show with jQuery
-  $('.leaflet-top').hide();
-  $('.leaflet-control-attribution').hide();
+// Hiding with jQuery to later show with jQuery
+$('.leaflet-top').hide();
+$('.leaflet-control-attribution').hide();
 
-  // Populates cordinates when you move the map on the explore screen
-  map.on('drag', onMapDrag);
+// Populates cordinates when you move the map on the explore screen
+map.on('drag', onMapDrag);
 
-  function onMapDrag(e) {
-    var center = map.getCenter();
-    var centerLat = center.lat.toFixed(2);
-    var centerLon = center.lng.toFixed(2);
-    $('#cords-lat').text(centerLat);
-    $('#cords-lon').text(centerLon);
-  }
+function onMapDrag(e) {
+  var center = map.getCenter();
+  var centerLat = center.lat.toFixed(2);
+  var centerLon = center.lng.toFixed(2);
+  $('#cords-lat').text(centerLat);
+  $('#cords-lon').text(centerLon);
+}
 
-
-
+// When the user hits the explore button
 function next(){
   $('#welcome').hide();
   $('#select').show();
@@ -37,6 +36,7 @@ function next(){
   // $('.leaflet-control-attribution').show();
 }
 
+// When the user hits the listen button
 function listen(){
   $('#select').hide();
   $('#listen').show();
@@ -45,14 +45,49 @@ function listen(){
   var center = map.getCenter();
   var lat = center.lat;
   var lon = center.lng;
-  console.log(lat + ", " + lon);
+  // console.log(lat + ", " + lon);
 
+  // Get Weather Data
   var weatherAPI = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=0d4413a00459125fa382c5085054f312";
-  // $.getJSON(weatherAPI, function(weatherData){
-  //   console.log(weatherData.name);
-  // });
+  $.getJSON(weatherAPI, function(weatherData){
 
-  var birdAPI = "http://www.xeno-canto.org/api/2/recordings?query=lat:" + lat + ",lon:" + lon + "callback=?";
+    console.log(weatherData);
+
+    // Wind descriptions based on the Beaufort scale
+    function describeWind(d) {
+        return d > 31  ? 'there is high wind. ' :
+               d > 25  ? 'there is a strong breeze. ' :
+               d > 18  ? 'there is a fresh breeze.	' :
+               d > 13  ? 'there is a moderate breeze. ' :
+               d > 8   ? 'there is a gentle breeze. ' :
+               d > 3   ? 'there is a light breeze. ' :
+               d > 0   ? 'the air is calm. ' :
+                         '';
+    }
+
+    // Wind descriptions based on the Beaufort scale
+    function describeRain(d) {
+        if (d.rain){
+          return "It's raining."
+        } else {
+          return ""
+        }
+    }
+
+
+
+
+    $('#soundscapeDiscription').html("In " + weatherData.name + ", "
+      + describeWind(weatherData.wind.speed)
+      + describeRain(weatherData)
+    );
+
+
+  });
+
+
+
+  // var birdAPI = "http://www.xeno-canto.org/api/2/recordings?query=lat:" + lat + ",lon:" + lon + "callback=?";
   // console.log('http://www.xeno-canto.org/api/2/recordings?query=box:45.5,-122.0,46.0,-121.0&callback=?');
   // $.getJSON('http://www.xeno-canto.org/api/2/recordings?query=box:45.5,-122.0,46.0,-121.0&callback=?', function(birdData){
   //   console.log(birdData);
@@ -64,3 +99,6 @@ function startOver() {
   $('#listen').hide();
   $('#welcome').show();
 }
+
+
+map.doubleClickZoom.disable();
