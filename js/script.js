cabbb0d3d351ae6c2f8c669318811eb27758a246
Jason <jasonbernert@gmail.com>
2017-01-13 18:31:32 -0800

    ditching the web audio API

--- js/script.js
+++ js/script.js
@@ -15,28 +15,21 @@ var map = L.map('map', {
 map.addLayer(Esri_WorldImagery);
 map.doubleClickZoom.disable();
 
-// Create audio context
-var AudioContext = window.AudioContext || window.webkitAudioContext;
-var audioCtx = new AudioContext();
-var bufferLoader;
-var masterGain = audioCtx.createGain();
-masterGain.gain.value = 0.8;
-masterGain.connect(audioCtx.destination);
-
 // Hiding with jQuery to later show with jQuery
 $('.leaflet-top').hide();
 $('.leaflet-control-attribution').hide();
 
 // Populates cordinates when you move the map on the explore screen
-map.on('drag', onMapDrag);
-
-function onMapDrag(e) {
-	var center = map.getCenter();
-	var centerLat = center.lat.toFixed(2);
-	var centerLon = center.lng.toFixed(2);
+function onMapDrag() {
+	const center = map.getCenter();
+	const centerLat = center.lat.toFixed(2);
+	const centerLon = center.lng.toFixed(2);
 	$('#cords-lat').text(centerLat);
 	$('#cords-lon').text(centerLon);
 }
+
+map.on('drag', onMapDrag);
+
 
 // When the user hits the explore button
 function next(){
@@ -60,21 +53,24 @@ function listen(){
 	var lon = center.lng;
 
 	// Format API calls with latitude and longitude
-	var weatherAPI = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=0d4413a00459125fa382c5085054f312";
+	const weatherAPI = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=0d4413a00459125fa382c5085054f312";
 
 	// Get Bird Data through proxy server because no CORS / JSONP supoort
-	var birdExampleAPI = "js/birdExampleData.json";
-	var birdAPI = "https://bird-sound-proxy.herokuapp.com/?url=http://www.xeno-canto.org/api/2/recordings?query=lat:" + lat + ",lon:" + lon;
-	var birdBoxAPI = "https://bird-sound-proxy.herokuapp.com/?url=http://www.xeno-canto.org/api/2/recordings?query=box:" + (lat - 0.5) + "," + (lon - 0.5) + "," + (lat + 0.5) + "," + (lon + 0.5);
+	const birdAPI = "https://bird-sound-proxy.herokuapp.com/?url=http://www.xeno-canto.org/api/2/recordings?query=box:" + (lat - 0.5) + "," + (lon - 0.5) + "," + (lat + 0.5) + "," + (lon + 0.5);
 
 	$.when(
 	    $.getJSON(weatherAPI),
-	    $.getJSON(birdBoxAPI)
+	    $.getJSON(birdAPI)
 	).done(function(rawWeatherData, rawBirdData) {
 
+		//
 		// Weather Data!
-		var weatherData = rawWeatherData[0];
-		// console.log(weatherData);
+		//
+
+		const weatherData = rawWeatherData[0];
+		const rain = weatherData.rain;
+		const wind = weatherData.wind.speed;
+
 		// Wind descriptions based on the Beaufort scale
 		function describeWind(d) {
 				return d > 31  ? 'there is high wind' :
@@ -84,107 +80,99 @@ function listen(){
 							 d > 8   ? 'there is a gentle breeze' :
 							 d > 3   ? 'there is a light breeze' :
 							 d > 0   ? 'the air is calm' :
-												 '';
+												 '' ;
 		}
 
-		function hearWind(d){
-			return d > 31  ? '../audio/wind/wind05.mp3' :
-						 d > 25  ? '../audio/wind/wind05.mp3' :
-						 d > 18  ? '../audio/wind/wind05.mp3' :
-						 d > 13  ? '../audio/wind/wind03.mp3' :
-						 d > 8   ? '../audio/wind/wind03.mp3' :
-						 d > 3   ? '../audio/wind/wind01.mp3' :
-						 d > 0   ? '../audio/wind/wind01.mp3' :
-											 '';
+		// load wind audio element based on wind speed
+		function hearWind(speed){
+			let windFile = '';
+			if(speed > 18){
+				windFile = '../audio/wind/wind05.mp3';
+			} else if (speed > 8) {
+				windFile = '../audio/wind/wind03.mp3';
+			} else{
+				windFile = '../audio/wind/wind01.mp3';
+			}
+			createAudio(windFile, 'wind');
 		}
 
 		// Rain description. Maybe make more descriptive based on amount of rain?
-		function describeRain(d) {
-				if (d.rain){
-					return ' and it&rsquo;s raining'
-				} else {
-					return ''
-				}
-		}
+		function describeRain(rain) {return rain ? ' and it&rsquo;s raining' : '';}
+		function hearRain(rain) {rain ? createAudio('../audio/rain/rain.mp3'): '';}
 
 
+		//
 		// Bird Data!
-		var birdData = rawBirdData[0];
+		//
 
-		// Bird description.
-		function describeBirds(d){
-			if (d.numRecordings > 0) {
-				console.log('Found ' + d.numRecordings + ' birds');
-				// console.log(d);
+		const birdData = rawBirdData[0];
+		const birdRecordings = birdData.recordings;
+		const uniqueBirds = [];
+		const usableBirds = [];
+		let birdList = [];
 
-				// Get unique Birds
-				var flags = [], birds = [], l = birdData.recordings.length, i;
-				for( i=0; i<l; i++) {
-				    if( flags[birdData.recordings[i].en]) continue;
-				    flags[birdData.recordings[i].en] = true;
-						birds.push({birdName: birdData.recordings[i].en.toLowerCase(), birdCall: birdData.recordings[i].file});
+		if (birdRecordings.length > 0) {
+
+			// Find unique birds
+			birdRecordings.forEach(bird => {
+				if(!uniqueBirds.includes(bird.en)){
+					uniqueBirds.push(bird.en);
+					usableBirds.push(bird);
 				}
-				console.log('There are ' + birds.length + ' unique birds.');
-				// console.log(birds);
+			});
 
-				if (birds.length >= 3) {
-					var bird1 = randomNumber(birds);
-					var bird2 = randomNumber(birds);
-					var bird3 = randomNumber(birds);
-					console.log('Reducing to three just birds, becuase I like the rule of threes.');
-					// $("#birdAudio").html('<audio><source src="' + birds[bird1].birdCall + '"></audio>');
-					createAudio(birds[bird1].birdCall);
+			// Pull three random birds
+			birdList = usableBirds.sort(() => 0.5 - Math.random()).slice(0,3);
 
+			// Create audio elements
+			birdList.forEach(bird => createAudio(bird.file, false));
 
-					return "You can hear the " + birds[bird1].birdName + ", "+ birds[bird2].birdName + " and " + birds[bird3].birdName + ".";
+			console.groupCollapsed('Bird data details');
+				console.log(birdData);
+				console.log(`Found ${birdRecordings.length} bird recordings.`);
+				console.log(`There are ${uniqueBirds.length} unique species.`);
+				console.log(`Here are ${birdList.length} random unique birds:`);
+				console.log(birdList);
+				console.groupEnd();
 
-				} else if (birds.length == 3){
-					return "You can hear the " + birds[0].birdName + ", " + birds[1].birdName + " and " + birds[2].birdName + ".";
-				} else if (birds.length == 2){
-					return "You can hear the " + birds[0].birdName + " and " + birds[1].birdName + ".";
-				} else if (birds.length == 1) {
-					return "You can hear the " + birds[0].birdName + ".";
-				}
-
-			} else {
-				return ""
-			}
+		} else {
+			console.error('There are no bird recordings for this area!')
 		}
+
+		// function describeBirds(){
+		//
+		// 	return "You can hear the " + birds[bird1].birdName + ", "+ birds[bird2].birdName + " and " + birds[bird3].birdName + ".";
+		//
+		// }
+
+
 
 		// Populate soundscape discription from data
-		$('#soundscapeDiscription').html("Here, "
-			+ describeWind(weatherData.wind.speed)
-			+ describeRain(weatherData)
-			+ ". "
-			+ describeBirds(birdData)
-		);
+		$('#soundscapeDiscription').html(`Here, ${describeWind(wind)} ${describeRain(rain)}.`);
+		// ${describeBirds}
 
-
-		// Create list of sound links for buffers
-		var bufferList = []
-		bufferList.push(hearWind(weatherData.wind.speed));
-		if (weatherData.rain){
-			bufferList.push('../audio/rain/rain.mp3');
-		}
-
-		// Load buffers
-		bufferLoader = new BufferLoader(audioCtx,bufferList,finishedLoading);
-		bufferLoader.load();
-
-		// Create AudioBufferSource Nodes
-		function finishedLoading(bufferList) {
-			for (var i = 0; i < bufferList.length; i++) {
-				var source = audioCtx.createBufferSource();
-				source.buffer = bufferList[i];
-				source.connect(masterGain);
-				masterGain.connect(audioCtx.destination);
-				source.loop = true;
-				source.start();
-			}
-		}
+		hearRain(rain);
+		hearWind(wind);
 
 	});
 }
+
+const soundscape = [];
+
+// Create an <audio> element and append it
+function createAudio(file, loop) {
+	const audio = new Audio();
+	audio.src = file;
+	audio.controls = false;
+	audio.loop = true;
+	audio.autoplay = true;
+	if (loop === false) {
+		audio.loop = false;
+	}
+	document.body.appendChild(audio);
+	soundscape.push(audio);
+}
+
 
 // When the user hits the "Search Again" button
 function startOver() {
@@ -192,25 +180,5 @@ function startOver() {
 		$('#soundscapeDiscription').html("");
 		$("#select").fadeIn( 400 );
 	});
-	masterGain.disconnect();
-}
-
-function randomNumber(d){
-	return Math.floor(Math.random() * d.length);
-}
-
-// Create an <audio> element and append to Bird Sounds div
-function createAudio(file) {
-	var audio = new Audio();
-	audio.src = file;
-	audio.controls = false;
-	audio.autoplay = true;
-	document.body.appendChild(audio);
-
-	audio.addEventListener('canplaythrough', function() {
-		var source = audioCtx.createMediaElementSource(audio);
-		console.log(source);
-		source.connect(masterGain);
-	}, false);
-
+	soundscape.forEach(sound => sound.remove());
 }
