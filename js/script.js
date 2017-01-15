95bbb6c1994b71b18279813d09afc36eb07b5ad7
Jason <jasonbernert@gmail.com>
2017-01-14 17:44:57 -0800

    trying dark sky's HTTPS weather API

--- js/script.js
+++ js/script.js
@@ -53,10 +53,10 @@ function listen(){
 	var lon = center.lng;
 
 	// Format API calls with latitude and longitude
-	const weatherAPI = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=0d4413a00459125fa382c5085054f312";
+	const weatherAPI = `https://bird-sound-proxy.herokuapp.com/?url=https://api.darksky.net/forecast/a98ca1129fb65f6b32ca53933b44222f/${lat},${lon}`;
 
 	// Get Bird Data through proxy server because no CORS / JSONP supoort
-	const birdAPI = "https://bird-sound-proxy.herokuapp.com/?url=http://www.xeno-canto.org/api/2/recordings?query=box:" + (lat - 0.5) + "," + (lon - 0.5) + "," + (lat + 0.5) + "," + (lon + 0.5);
+	const birdAPI = `https://bird-sound-proxy.herokuapp.com/?url=http://www.xeno-canto.org/api/2/recordings?query=box:${lat - 0.5},${lon - 0.5},${lat + 0.5},${lon + 0.5}`;
 
 	$.when(
 	    $.getJSON(weatherAPI),
@@ -66,10 +66,9 @@ function listen(){
 		//
 		// Weather Data!
 		//
-
-		const weatherData = rawWeatherData[0];
-		const rain = weatherData.rain;
-		const wind = weatherData.wind.speed;
+		const weatherData = rawWeatherData[0].currently;
+		const rain = weatherData.precipIntensity;
+		const wind = weatherData.windSpeed;
 
 		// Wind descriptions based on the Beaufort scale
 		function describeWind(d) {
@@ -90,16 +89,17 @@ function listen(){
 				windFile = '../audio/wind/wind05.mp3';
 			} else if (speed > 8) {
 				windFile = '../audio/wind/wind03.mp3';
-			} else{
+			} else {
 				windFile = '../audio/wind/wind01.mp3';
 			}
 			createAudio(windFile, 'wind');
 		}
 
 		// Rain description. Maybe make more descriptive based on amount of rain?
-		function describeRain(rain) {return rain ? ' and it&rsquo;s raining' : '';}
-		function hearRain(rain) {rain ? createAudio('../audio/rain/rain.mp3'): '';}
-
+		function describeRain(rain) {return rain > 0 ? ' and it&rsquo;s raining' : '';}
+		function hearRain(rain) {rain > 0 ? createAudio('../audio/rain/rain.mp3'): '';}
+		hearRain(rain);
+		hearWind(wind);
 
 		//
 		// Bird Data!
@@ -110,12 +110,13 @@ function listen(){
 		const uniqueBirds = [];
 		const usableBirds = [];
 		let birdList = [];
+		let birdDescription = '';
 
 		if (birdRecordings.length > 0) {
 
 			// Find unique birds
 			birdRecordings.forEach(bird => {
-				if(!uniqueBirds.includes(bird.en)){
+				if(!uniqueBirds.includes(bird.en) && (bird.en).includes != 'unknown'){
 					uniqueBirds.push(bird.en);
 					usableBirds.push(bird);
 				}
@@ -124,14 +125,20 @@ function listen(){
 			// Pull three random birds
 			birdList = usableBirds.sort(() => 0.5 - Math.random()).slice(0,3);
 
+			if (birdList.length == 3){
+				birdDescription = `You can hear the ${birdList[0].en}, ${birdList[1].en} and ${birdList[2].en}.`;
+			} else if (birdList.length == 2){
+				birdDescription = `You can hear the ${birdList[0].en} and ${birdList[1].en}.`;
+			}	else if (birdList.length == 1){
+				birdDescription = `You can hear the ${birdList[0].en}.`;
+			}
+
 			// Create audio elements
 			birdList.forEach(bird => createAudio(bird.file, false));
 
 			console.groupCollapsed('Bird data details');
 				console.log(birdData);
-				console.log(`Found ${birdRecordings.length} bird recordings.`);
-				console.log(`There are ${uniqueBirds.length} unique species.`);
-				console.log(`Here are ${birdList.length} random unique birds:`);
+				console.log(`Out of ${birdRecordings.length} bird recordings there are ${uniqueBirds.length} unique species. Here are ${birdList.length} random unique birds:`);
 				console.log(birdList);
 				console.groupEnd();
 
@@ -139,40 +146,24 @@ function listen(){
 			console.error('There are no bird recordings for this area!')
 		}
 
-		// function describeBirds(){
-		//
-		// 	return "You can hear the " + birds[bird1].birdName + ", "+ birds[bird2].birdName + " and " + birds[bird3].birdName + ".";
-		//
-		// }
-
-
-
 		// Populate soundscape discription from data
-		$('#soundscapeDiscription').html(`Here, ${describeWind(wind)} ${describeRain(rain)}.`);
-		// ${describeBirds}
-
-		hearRain(rain);
-		hearWind(wind);
+		$('#soundscapeDiscription').html(`Here, ${describeWind(wind)}${describeRain(rain)}. ${birdDescription}`);
 
 	});
 }
 
-const soundscape = [];
+let soundscape = [];
 
 // Create an <audio> element and append it
 function createAudio(file, loop) {
 	const audio = new Audio();
 	audio.src = file;
 	audio.controls = false;
-	audio.loop = true;
+	audio.loop = loop;
 	audio.autoplay = true;
-	if (loop === false) {
-		audio.loop = false;
-	}
 	document.body.appendChild(audio);
 	soundscape.push(audio);
 }
-
 
 // When the user hits the "Search Again" button
 function startOver() {
@@ -181,4 +172,5 @@ function startOver() {
 		$("#select").fadeIn( 400 );
 	});
 	soundscape.forEach(sound => sound.remove());
+	soundscape = [];
 }
